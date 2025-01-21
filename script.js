bfa93060bfa5288a7fd62b6ee80884272e7e451d
MonishBadveli <badvelimkreddy@gmail.com>
2025-01-21 15:40:35 +0530

    hmm

--- script.js
+++ script.js
@@ -31,5 +31,61 @@ function getUserChoice()
     }
 
 }
-console.log(getComputerChoice());
-console.log(getUserChoice());
+//console.log(getComputerChoice());
+//console.log(getUserChoice());
+
+let computerScore=0;
+let humanScore=0;
+
+function playRound()
+{
+    let humanChoice=getUserChoice();
+    let computerChoice=getComputerChoice();
+    let winMessage = () => alert("You picked " + humanChoice + " and the computer picked " + computerChoice + ". You win this round!");
+    let loseMessage = () => alert("You picked " + humanChoice + " and the computer picked " + computerChoice + ". You lose this round!");
+    let tieMessage = () => alert("You picked " + humanChoice + " and the computer picked " + computerChoice + ". It's a tie!");
+    if(humanChoice==='rock' && computerChoice==='scissors'){
+        winMessage();
+        humanScore++;
+    }
+    else if(humanChoice==='paper' && computerChoice==='rock'){
+        winMessage();
+        humanScore++;
+    }
+    else if(humanChoice==='scissors' && computerChoice==="paper"){
+        winMessage();
+        humanScore++;
+    }
+    else if(humanChoice === computerChoice){
+        tieMessage();
+    }
+    else{
+        loseMessage();
+        computerScore++;
+    }
+
+}
+function playGame()
+{
+
+    for (let i = 0; i < 5; i++) {
+        playRound();
+
+    }
+
+    let isWinner = humanScore > computerScore;
+    let isTie = humanScore === computerScore;
+
+    if (isWinner) {
+        alert("You win the game!");
+    }
+    else if (isTie) {
+        alert("The game is a tie!");
+    } 
+    else {
+        alert("You lost the game!");
+    }
+
+
+}
+playGame();
